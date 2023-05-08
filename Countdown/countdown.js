ac713bf1adca985c7b20b126e5e67aa00a082da0
BeltserG <beltserg@icloud.com>
2023-05-09 00:10:17 +0500

    Initial commit

---
+++ Countdown/countdown.js
@@ -0,0 +1,26 @@
+const launchDay = "1 Sep 2023";
+
+const daysElement = document.getElementById("days")
+const hoursElement = document.getElementById("hours")
+const minutesElement = document.getElementById("minutes")
+const secondsElement = document.getElementById("seconds")
+
+function left(){
+    let currentDate = new Date();
+    let launchDate = new Date(launchDay);
+    const secondsTotal = Math.floor((launchDate - currentDate) / 1000);
+    const days = Math.floor(secondsTotal / (3600*24));
+    const hours = Math.floor(secondsTotal / 3600 % 24);
+    const minutes = Math.floor(secondsTotal / 60 % 60);
+    const seconds = secondsTotal % 60;
+
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = hours;
+    minutesElement.innerHTML = minutes;
+    secondsElement.innerHTML = seconds;
+
+}
+
+left();
+
+setInterval(left, 1000);
