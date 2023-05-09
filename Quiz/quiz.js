dccef62956cb2f6c81e1c4e1bdd4baecbf4cf295
BeltserG <beltserg@icloud.com>
2023-05-09 15:35:42 +0500

    Initial commit Quiz

---
+++ Quiz/quiz.js
@@ -0,0 +1,64 @@
+const quizData = [{
+    question: "What drink is the most popular in the morning?",
+    an_a: "tea",
+    an_b: "milk",
+    an_c: "coffee",
+    an_d: "vodka",
+    correct: "an_c"
+},
+{
+    question: "What is the country with the largest population?",
+    an_a: "China",
+    an_b: "India",
+    an_c: "USA",
+    an_d: "Russia",
+    correct: "an_b"
+},
+{
+    question: "What is the most popular programming language?",
+    an_a: "JacaScript",
+    an_b: "Python",
+    an_c: "C",
+    an_d: "SQL",
+    correct: "an_a"
+},
+{
+    question: "What is the largest company in the world by capitalisation?",
+    an_a: "McDonald's",
+    an_b: "Coca Cola",
+    an_c: "Tesla",
+    an_d: "Apple",
+    correct: "an_d"
+}];
+
+let currentQ = 0;
+const Qtext = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const btn = document.getElementById("btn");
+
+loadQuiz();
+
+function loadQuiz(){
+    const currentQuiz = quizData[currentQ];
+    Qtext.innerHTML = currentQuiz.question;
+    a_text.innerHTML = currentQuiz.an_a;
+    b_text.innerHTML = currentQuiz.an_b;
+    c_text.innerHTML = currentQuiz.an_c;
+    d_text.innerHTML = currentQuiz.an_d;
+}
+
+btn.addEventListener("click", () =>{
+    currentQ++;
+    // while(true){
+
+    // }
+    if (currentQ < quizData.length){
+        loadQuiz();
+    }
+    else{
+        alert("Done!");
+    }
+});
