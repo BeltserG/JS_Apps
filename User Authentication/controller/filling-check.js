65769cf4202438ea2ed3c6146668044084bd2ea0
BeltserG <beltserg@icloud.com>
2023-05-28 19:47:12 +0500

    Placeholder feature

--- User Authentication/controller/filling-check.js
+++ User Authentication/controller/filling-check.js
@@ -1,13 +1,31 @@
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 const submit = document.getElementById("auth-submit");
-
 const userData = [username,password];
 
-submit.addEventListener("click", () => {
-    userData.forEach(item =>{
-        if(item.value.trim() === ""){
-            item.parentElement.style.border = "2px solid rgb(187, 24, 24)";
-        };
+function fieldCheck(submit, userData){
+    userData.forEach(data =>{
+        let placeholder = data.placeholder
+        data.addEventListener("click", () =>{
+            if (data.placeholder === ""){
+                data.placeholder = placeholder;
+            }
+            else{
+                data.placeholder = "";
+            }
+        })
     })
-})
+    submit.addEventListener("click", (e) => {
+        e.preventDefault();
+        userData.forEach(item =>{
+            if(item.value.trim() === ""){
+                item.parentElement.classList.add("error-border");
+            }
+            else{
+                item.parentElement.classList.remove("error-border");
+            }
+        })
+    })
+}
+
+fieldCheck(submit, userData);
